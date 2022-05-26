fc65fe2df856d2cdc03b5a8ad176b3d613dbb81f
Robertsnemeth <104597057+Robertsnemeth@users.noreply.github.com>
2022-05-26 12:19:13 -0700

    update

--- Test and Optional Assignments/JStests/script.js
+++ Test and Optional Assignments/JStests/script.js
@@ -1,3 +1,17 @@
 function turnoff(element) {
     element.innerText = "Off";
 }
+
+let Yankees = {
+    Catcher: "Kyle Higashioka",
+    Firstbase: "Anthony Rizzo",
+    Secondbase: "DJ LeMahieu",
+    Shortstop: "Isiah Kiner-Falefa",
+    Thirdbase: "Josh Donaldson",
+    RightField: "Aaron Judge",
+    CenterField: "Aaron Hicks",
+    LeftField: "Joey Gallo",
+    DH: "Giancarlo Stanton"
+}
+
+console.log(Yankees.Shortstop)
